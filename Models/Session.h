061565424cb4c148d73992612e790352081abf4e
Andy Styles <andy.styles@btinternet.com>
2025-09-26 16:40:56 +0100

    Refresh copyright, license and disclaimer message
    Reformat header comment to be consistent, and correct dates for new
    files.

--- Models/Session.h
+++ Models/Session.h
@@ -1,19 +1,20 @@
 /*
 
-Copyright Ⓒ 2023 Andy Styles
+Copyright Ⓒ 2025 Andy Styles
 All Rights Reserved
-
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
 
  * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
+   notice, this list of conditions and the following disclaimer.
+
  * Redistributions in binary form must reproduce the above copyright
-     notice, this list of conditions and the following disclaimer in
-     the documentation and/or other materials provided with the
-     distribution.
+   notice, this list of conditions and the following disclaimer in
+   the documentation and/or other materials provided with the
+   distribution.
+
  * Neither the name of The Qt Company Ltd nor the names of its
      contributors may be used to endorse or promote products derived
      from this software without specific prior written permission.
--- ProcessDataStream.h
+++ ProcessDataStream.h
@@ -9,10 +9,12 @@ met:
 
  * Redistributions of source code must retain the above copyright
    notice, this list of conditions and the following disclaimer.
+
  * Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in
    the documentation and/or other materials provided with the
    distribution.
+
  * Neither the name of The Qt Company Ltd nor the names of its
    contributors may be used to endorse or promote products derived
    from this software without specific prior written permission.
@@ -30,7 +32,6 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-
 
 #ifndef PROCESSDATASTREAM_H
 #define PROCESSDATASTREAM_H
